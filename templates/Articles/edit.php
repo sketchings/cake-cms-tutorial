1bc6516badcaf1891e83db1b1540a6137de11076
Alena Holligan <alena@smartours.com>
2023-10-30 01:42:32 -0700

    Update files to fix application

--- templates/Articles/edit.php
+++ templates/Articles/edit.php
@@ -4,6 +4,7 @@
 <?php
     echo $this->Form->create($article);
     echo $this->Form->control('title');
+    echo $this->Form->control('slug');
     echo $this->Form->control('body', ['rows' => '3']);
     echo $this->Form->control('tag_string', ['type' => 'text']);
     echo $this->Form->button(__('Save Article'));
